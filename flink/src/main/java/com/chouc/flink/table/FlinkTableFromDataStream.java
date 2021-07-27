7e481c8b98da93f25e30bcc755fb61ddea1aaecd
chouc <chouc.mei@gmail.com>
2021-07-27 22:50:47 +0800

    flink-table-stream table from datastream

--- flink/src/main/java/com/chouc/flink/table/FlinkTableFromDataStream.java
+++ flink/src/main/java/com/chouc/flink/table/FlinkTableFromDataStream.java
@@ -4,13 +4,25 @@ import com.chouc.flink.utils.StreamSourceUtils;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SocketTextStreamFunction;
+import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import static org.apache.flink.table.api.Expressions.$;
 
 public class FlinkTableFromDataStream {
     public static void main(String[] args) {
         StreamExecutionEnvironment streamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment streamTableEnvironment = StreamTableEnvironment.create(streamExecutionEnvironment);
         DataStreamSource<String> socketStream = StreamSourceUtils.getSocketStream();
-        streamTableEnvironment.fromDataStream(socketStream).execute().print();
+/**
+ *      streamTableEnvironment.fromDataStream(socketStream).execute().print();
+ *
+ *      streamTableEnvironment.fromDataStream(socketStream, Schema.newBuilder()
+ *              .column("f0", "String")
+ *              .build()).execute().print();
+ */
+
+        streamTableEnvironment.fromDataStream(socketStream, $("col")).execute().print();
+
     }
 }
