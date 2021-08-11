dfd48a8e88e5d0ecd8da4ef68a6e5dbba4221857
chouc <chouc.mei@gmail.com>
2021-08-11 22:55:34 +0800

    flink-time-attribute row time and process time

--- flink/src/main/java/com/chouc/flink/table/FlinkTableFromDataStreamTimeAttributeProcessTime.java
+++ flink/src/main/java/com/chouc/flink/table/FlinkTableFromDataStreamTimeAttributeProcessTime.java
@@ -3,12 +3,10 @@ package com.chouc.flink.table;
 import com.chouc.flink.utils.StreamSourceUtils;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.ApiExpression;
-import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 import static org.apache.flink.table.api.Expressions.$;
-
+// https://ci.apache.org/projects/flink/flink-docs-release-1.13/docs/dev/table/concepts/time_attributes/
 public class FlinkTableFromDataStreamTimeAttributeProcessTime {
     public static void main(String[] args) {
         StreamExecutionEnvironment streamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
